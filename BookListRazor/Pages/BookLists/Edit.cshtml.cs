9a6fb11378b67041a161b44c8b24027745247c0a
Jeffery Vincent <jeffery.vicnent@msn.com>
2020-12-12 14:26:00 -0500

    Add project files.

---
+++ BookListRazor/Pages/BookLists/Edit.cshtml.cs
@@ -0,0 +1,51 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BookListRazor.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace BookListRazor.Pages.BookLists
+{
+    public class EditModel : PageModel
+    {
+        private ApplicationDbContext _db;
+
+        public EditModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        [BindProperty]
+        public Book Book { get; set; }
+
+
+        public async Task OnGet(int id)
+        {
+            Book = await _db.Books.FindAsync(id);
+        }
+
+
+        public async Task<IActionResult> OnPost()
+        {
+            if(ModelState.IsValid)
+            {
+                var BookFromDB = await _db.Books.FindAsync(Book.Id);
+                BookFromDB.Name = Book.Name;
+                BookFromDB.Author = Book.Author;
+                BookFromDB.ISBN = Book.ISBN;
+
+                await _db.SaveChangesAsync();
+
+                return RedirectToPage("Index");
+            }
+            else
+            {
+                return RedirectToPage();
+            }
+        }
+            
+    }
+}
+
